db57f46b88c1ef2c95f11d235c9215da42addde3
Andrew Bliss <andrewgbliss@gmail.com>
2015-11-10 16:40:09 -0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # you-just-purged-morty
 A node module that will occasionally purge a directory you specify.
+
+#Usage
+
+```bash
+node index.js -d /some/dir -i 10000
+```
+
+#Notes
+Usage: index.js -d [directory to purge] -i [interval in milliseconds]
---
+++ index.js
@@ -0,0 +1,15 @@
+var _ = require('lodash');
+var del = require('del');
+var args = require('yargs')
+  .usage('Usage: $0 -d [directory to purge] -i [interval in milliseconds]')
+  .demand(['d', 'i'])
+  .argv;
+
+setInterval(function() {
+  del(args.d)
+    .then(function() {
+      console.log('You just purged Morty!');
+    });
+}, args.i);
+
+console.log(args.d);
